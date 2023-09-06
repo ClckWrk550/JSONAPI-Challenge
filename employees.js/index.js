dd9f3ae6879e57fae89675560ffee1c5e850cc52
ClckWrk550 <72582066+ClckWrk550@users.noreply.github.com>
2023-09-06 16:16:38 -0700

    Update index.js

--- employees.js/index.js
+++ employees.js/index.js
@@ -40,8 +40,8 @@ async function getEmployees(order = null) {
             const aSalary = parseFloat(a.salary) || -1;
             const bSalary = parseFloat(b.salary) || -1;
 
-            if (order === 'asc') return aSalary - bSalary;
-            if (order === 'desc') return bSalary - aSalary;
+            if (order === 'a') return aSalary - bSalary;
+            if (order === 'd') return bSalary - aSalary;
             return 0;
         });
 
@@ -54,4 +54,4 @@ async function getEmployees(order = null) {
     }
 }
 
-getEmployees('asc'); // Inputs options are 'asc' and 'desc'
+getEmployees('a'); // Inputs options are 'a' and 'd'
