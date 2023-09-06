137b1804da459c5d106effa219bc7f6c2d602593
ClckWrk550 <72582066+ClckWrk550@users.noreply.github.com>
2023-09-06 16:42:47 -0700

    Update index.js

--- employees.js/index.js
+++ employees.js/index.js
@@ -5,7 +5,7 @@ const domain = './employees.js/fake-server';
 // Simple Json parsing
 async function getEmplFrom(file) {
     try {
-        const data = await fs.readFile(file, 'utf-8');
+        const data = await fs.readFile(file);
         return JSON.parse(data);
     } catch (error) {
         console.error(`Error : `, error);
@@ -14,7 +14,7 @@ async function getEmplFrom(file) {
 }
 
 async function getSalaries() {
-    const s = await getEmplFrom(domain + '/salaries.json');
+    const s = await getEmplFrom(domain + `/salaries.json`);
     //console.log(s); // DEBUG
     return s;
 }
@@ -22,26 +22,26 @@ async function getSalaries() {
 
 async function getEmployees(order = null) {
     try {
-        const employees = await getEmplFrom(domain + '/employees.json');
+        const employees = await getEmplFrom(domain + `/employees.json`);
         const salaries = await getSalaries();
 
         // Finding the employees by salary
         const data = employees.map(emp => {
-            const salaryItem = salaries.find(s => s.employeeId === emp.id);
+            const search = salaries.find(s => s.employeeId === emp.id);
             return {
                 id: emp.id,
                 name: emp.name,
-                salary: (salaryItem && salaryItem.salary) || null
+                salary: (search && search.salary) || null
             };
         });
 
         // Options for sorting data by ascending, descending salary
         data.sort((a, b) => {
-            const aSalary = parseFloat(a.salary) || -1;
-            const bSalary = parseFloat(b.salary) || -1;
+            const aSal = parseFloat(a.salary) || -1;
+            const bSal = parseFloat(b.salary) || -1;
 
-            if (order === 'asc') return aSalary - bSalary;
-            if (order === 'desc') return bSalary - aSalary;
+            if (order === 'asc') return aSal - bSal;
+            if (order === 'desc') return bSal - aSal;
             return 0;
         });
 
